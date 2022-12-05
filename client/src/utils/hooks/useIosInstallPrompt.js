57a91d8f7978281ba824590b176788e7b29a11fa
host4ideas <felixmb99@hotmail.es>
2022-12-05 12:36:52 +0100

    añadidos cambios rama felix

---
+++ client/src/utils/hooks/useIosInstallPrompt.js
@@ -0,0 +1,26 @@
+import useShouldShowPrompt from "./useShouldShowPrompt";
+
+const iosInstallPromptedAt = "iosInstallPromptedAt";
+
+const isIOS = () => {
+    // @ts-ignore
+    if (navigator.standalone) {
+        //user has already installed the app
+        return false;
+    }
+    const ua = window.navigator.userAgent;
+    const isIPad = !!ua.match(/iPad/i);
+    const isIPhone = !!ua.match(/iPhone/i);
+    return isIPad || isIPhone;
+};
+
+const useIosInstallPrompt = () => {
+    const [userShouldBePromptedToInstall, handleUserSeeingInstallPrompt] =
+        useShouldShowPrompt(iosInstallPromptedAt);
+
+    return [
+        isIOS() && userShouldBePromptedToInstall,
+        handleUserSeeingInstallPrompt,
+    ];
+};
+export default useIosInstallPrompt;
